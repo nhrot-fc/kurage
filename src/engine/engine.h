9f884354bdf0206a7b7d3e28bd5b0cc2d34bbaa2
nhrot-fc <a20213812@pucp.edu.pe>
2025-10-11 15:27:08 -0500

    Added some references

--- src/engine/engine.h
+++ src/engine/engine.h
@@ -1,6 +1,44 @@
 #ifndef ENGINE_H
 #define ENGINE_H
 
-void engine_show_constants();
+typedef struct {
+  float x;
+  float y;
+} Vector2;
+
+typedef struct {
+  Vector2 position;
+} PositionComponent;
+
+typedef struct {
+  Vector2 velocity;
+  Vector2 force_accumulator;
+  float mass;
+} PhysicsComponent;
+
+typedef struct {
+  float density;
+  float pressure;
+} FluidParticleComponent;
+
+typedef struct {
+  int entity_count;
+  int capacity;
+
+  unsigned int *entity_masks;
+  PositionComponent *positions;
+  PhysicsComponent *physics;
+  FluidParticleComponent *fluid_particles;
+
+} Universe;
+
+Universe *create_world(int capacity);
+void destroy_world(Universe *world);
+int create_particle(Universe *world, Vector2 pos, float mass);
+
+void Gravity_update(Universe *world);
+void Fluid_update(Universe *world);
+void Movement_update(Universe *world, float dt);
+void ForceCleanup_update(Universe *world);
 
 #endif // ENGINE_H
